da91f6e5d48e32e6653731040fc579638b3855db
Abel Arana <abel.arana@hotmail.com>
2023-12-14 12:12:18 -0500

    init task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::create([
+            'name' => 'Abel Arana',
+            'email' => 'abel.arana@hotmail.com',
+            'password' => bcrypt('lobomalo123'),
+        ]);
     }
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+@stop
+@section('content')
+    <div class="container-fluid">
+        {{ $slot }} 
+    </div>
+    @livewire('wire-elements-modal')
+    
+@stop
+@section('css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+    <x-livewire-alert::scripts />
+@stop
